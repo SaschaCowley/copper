d47f88296e5be4c6db199fa8a7a1836982b8f945
Sascha <16543535+SaschaCowley@users.noreply.github.com>
2026-06-18 13:11:14 +1000

    Fix accidentally included target dir

--- libcopper/target/debug/deps/unicode_ident-b933eb70f68a63d1.d
+++
@@ -1,6 +0,0 @@
-D:\projects\copper\libcopper\target\debug\deps\unicode_ident-b933eb70f68a63d1.d: C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-D:\projects\copper\libcopper\target\debug\deps\libunicode_ident-b933eb70f68a63d1.rmeta: C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\Sascha\scoop\persist\rustup\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
